138a2a4faa4ab20620c16716851b4b4665071cdf
Hook <hookhookun@gmail.com>
2020-05-11 11:53:58 +0900

    Fix index

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,7 @@
 export * from './BinaryMatrix';
+export * from './checkExpansion';
+export * from './indicesToOffset';
+export * from './isSame';
+export * from './offsetToIndices';
+export * from './sizeToBitLength';
+export * from './type';
